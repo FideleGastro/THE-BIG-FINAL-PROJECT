5c0cc26cd91dd6c384f746eeab4bb26185d3f03d
FidèleGastro <gomes.raphael.31@gmail.com>
2017-11-03 16:48:36 +0100

    query no option

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,23 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+
+        if ($request->isMethod('POST'))
+            $valid = $request->get('query');
+        else
+            $valid = 'no';
+        $response = $this->forward('MyBioApiBundle:Default:get', array(
+            'query'  => 'toto',
+        ));
+        $response = json_decode($response->getContent(), true);
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'request' => $response,
+            'test' => $valid
         ]);
     }
 }
--- src/MyBioApiBundle/Controller/DefaultController.php
+++ src/MyBioApiBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace MyBioApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,29 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('MyBioApiBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/api/query/{query}")
+     */
+    public function getAction($query)
+    {
+        $json_url = "https://www.ebi.ac.uk/europepmc/webservices/test/rest/search?query=".$query."&format=json&resulttype=core";
+        $json = file_get_contents($json_url);
+        $data = json_decode($json, TRUE);
+        $data = new JsonResponse($data);
+        return $data;
+    }
+
+    /**
+     * @Route("/api/query/{query}/{option}")
+     */
+    public function sgetAction($query, $option)
+    {
+        $json_url = "https://www.ebi.ac.uk/europepmc/webservices/test/rest/search?query=".$option.":".$query."&format=json&resulttype=core";
+        $json = file_get_contents($json_url);
+        $data = json_decode($json, TRUE);
+        $data = new JsonResponse($data);
+        return $data;
     }
 }
