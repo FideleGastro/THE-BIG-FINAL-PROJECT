042e9d6fa82b05904ab52cb4cde3e9c0acc1e747
FidèleGastro <gomes.raphael.31@gmail.com>
2017-11-19 18:37:35 +0100

    css

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,19 +13,26 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
         if ($request->isMethod('POST'))
             $valid = $request->get('query');
         else
-            $valid = 'no';
+            $valid = 'toto';
         $response = $this->forward('MyBioApiBundle:Default:get', array(
-            'query'  => 'toto',
+            'query'  => $valid,
         ));
         $response = json_decode($response->getContent(), true);
-        return $this->render('default/index.html.twig', [
+
+        if ($valid == "toto")
+            return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'request' => $response,
-            'test' => $valid
-        ]);
+            ]);
+        else
+            return $this->render('default/result.html.twig', [
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+                'request' => $response,
+                'test' => $valid
+            ]);
+
+
     }
 }
